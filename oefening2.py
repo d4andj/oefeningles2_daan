5bf43e219b67c1e8bd7071baf146795093b29bac
Daan <daandj@gmail.com>
2023-12-07 20:35:14 +0100

    Update oefening2.py

--- oefening2.py
+++ oefening2.py
@@ -2,4 +2,8 @@ getal1 = input("geef het eerste getal op: ")
 getal2 = input("geef het tweede getal op: ")
 
 som = int(getal1) + int(getal2) + 4
-print(som)
+product = int(getal1) * int(getal2)
+
+print("de som van de getallen is: " , som)
+print("het product van de getallen is: " , product)
+
